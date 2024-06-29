d13bf19c15a968249d05bb6c3bd9f7361be06b8b
Ordeeper <pedrordeeper@gmail.com>
2024-06-29 19:47:27 -0300

    feat: Add initial structure

---
+++ components/MagicCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="m-7">
+        <div class="flex-col max-w-xs border border-black rounded-lg">
+            <img class="rounded-lg" src="https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027-630x420.jpg">
+            <div class="flex flex-col py-4 px-7 gap-5">
+                <div class="text-sm items-center font-mono flex justify-between">
+                    <span>Dezembro 4, 2023</span>
+                    <span class="border border-black py-1 px-3 rounded-xl">Anime</span>
+                </div>
+                <h3 class="text-xl">
+                    Unraveling the Narrative Mastery of Manga
+                </h3>
+                <p class="font-serif">Dive into the captivating world of anime, from its rich history to the latest trends and fan-favorite
+                    series...
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ composables/useMousePosition.js
@@ -0,0 +1,23 @@
+// composables/useMousePosition.js
+import { ref, onMounted, onUnmounted } from 'vue';
+
+export function useMousePosition() {
+  const x = ref(0);
+  const y = ref(0);
+
+  const updateMousePosition = (event) => {
+    x.value = event.clientX;
+    y.value = event.clientY;
+  };
+
+  onMounted(() => {
+    window.addEventListener('mousemove', updateMousePosition);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', updateMousePosition);
+  });
+
+  return { x, y };
+}
+
